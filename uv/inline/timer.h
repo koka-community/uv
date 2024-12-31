09925dcdf32c2a8f6618120acabbea66e4dbfc9f
Tim Whiting <tim@whitings.org>
2024-12-30 20:16:39 -0700

    Transfer from PR

--- uv/inline/timer.h
+++ uv/inline/timer.h
@@ -5,4 +5,18 @@
    use this file except in compliance with the License. A copy of the License
    can be found in the LICENSE file at the root of this distribution.
 ----------------------------------------------------------------------------*/
-import std/test
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+
+typedef struct kk_wasm_timer_s {
+  kk_function_t callback;
+  kk_box_t kkhandle;
+  int64_t repeat_ms;
+  int timer;
+} kk_wasm_timer_t;
+
+EMSCRIPTEN_KEEPALIVE void wasm_timer_callback(kk_wasm_timer_t* timer_info);
+#else
+#include <uv.h>
+#endif
